fb8cb3646f8c08f2561d55737851efad87d26a9e
sangwonKim <llllssss94@naver.com>
2018-11-06 21:50:48 +0900

    add json format

--- neo4j-service.js
+++ neo4j-service.js
@@ -3,11 +3,23 @@ const neo4j = require('./neo4j');
 exports.getCountByIP = async function(destIp) {
     return new Promise((resolve, reject) => {
         let returnPromise = neo4j.session.run(
-            'MATCH (node:test)-[r]-() RETURN node, r',
+            'MATCH (node:test)-[edge]-() RETURN node, edge',
             {ip: destIp}
         );
         returnPromise.then(result => {
-            resolve(JSON.stringify(result.records));
+            console.log(result.records);
+            let nodeArray = new Array();
+            let edgeArray = new Array();
+            let data = JSON.parse(JSON.stringify(result.records));
+            for(let i = 0; i < data.length; i++){
+                nodeArray.push(data[i]['_fields'][0]['properties']);
+                edgeArray.push(data[i]['_fields'][1]['properties']);
+            }
+            let ret = new Object();
+            ret.node = nodeArray;
+            ret.edge = edgeArray;
+            ret = JSON.stringify(ret);
+            resolve(ret);
         })
     })
 }
